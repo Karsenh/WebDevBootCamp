51c352bea30dbf14812120bb271b5de8df36ed9b
Karsenh <KarsenHansen@gmail.com>
2021-02-08 02:45:37 -0800

    Fixed login and register post routes

--- S.32 - Auth & Security/Secretes/app.js
+++ S.32 - Auth & Security/Secretes/app.js
@@ -5,10 +5,12 @@ const express = require('express');
 const bodyParser = require('body-Parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
+// express-session for session management
 const session = require('express-session');
-const password = require('passport');
-const passportLocalMongoose = require('passport-local-mongoose');
+// passport for authentication
 const passport = require('passport');
+// passportLocalMongoose to simply building username & password login with Passport
+const passportLocalMongoose = require('passport-local-mongoose');
 /**
  * Old techqniue using bcrypt (see update with passport)
  */
@@ -16,9 +18,11 @@ const passport = require('passport');
 // const bcrypt = require('bcrypt');
 // const saltRounds = 10;
 
-
 const app = express();
 
+/**
+ * MIDDLEWARE
+ */
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
@@ -41,17 +45,20 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
+// Fix deprecation warning
 mongoose.set("useCreateIndex", true);
 
+/**
+ * DB SCHEMA
+ */
 // Create an object from the Mongoose Schema class
 const userSchema = new mongoose.Schema({
     email: String,
     password: String
 });
 
-// Add a plugin to our mongoose user schema (must be mongoose)
+// Add and enable plugin to our mongoose user schema (must be mongoose)
 userSchema.plugin(passportLocalMongoose);
-
 
 // Add encyrption plugin to user schema and define the field using the secret to encrypt
 // Mongoose will Encrypt while calling 'save' and Decrypt while calling 'find'
@@ -60,9 +67,13 @@ userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
 
+// Local strategy to authenticate users using their username and password
 passport.use(User.createStrategy());
 
+// Only necessary when using sessions || much shorter using passport-local-mongoose
+// Serializing users creates 'fortune cookie', stuffing info into the cookie
 passport.serializeUser(User.serializeUser());
+// Allows passport to 'crumble' the cookie to attain info in cookie
 passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function (req, res) {
@@ -77,9 +88,27 @@ app.get("/register", function (req, res) {
     res.render("register");
 });
 
+app.get("/secrets", function(req, res) {
+    if(req.isAuthenticated()) {
+        res.render("secrets");
+    } else {
+        res.redirect("/login");
+    }
+});
+
 app.post("/register", function (req, res) {
 
-
+    User.register({ username: req.body.username }, req.body.password, function(err, user) {
+        if (err) {
+            console.log(err);
+            res.redirect("/register");
+        } else {
+            passport.authenticate("local")(req, res, function() {
+                // Successfully authenticated and established a cookie
+                res.redirect("/secrets");
+            });
+        }
+    });
 
     /**
      * Old Technique using bcrypt (see above for passport)
@@ -89,7 +118,7 @@ app.post("/register", function (req, res) {
     //         email: req.body.username,
     //         password: hash
     //     });
-    //     // Mongoose will encrypt password at this stage
+        // Mongoose will encrypt password at this stage
     //     newUser.save(function (err) {
     //         if (err) {
     //             console.log(err);
@@ -102,17 +131,29 @@ app.post("/register", function (req, res) {
 
 app.post("/login", function (req, res) {
 
+    const user = new User({
+        username: req.body.username,
+        password: req.body.password
+    });
     
-
+    req.login(user, function(err) {
+        if (err) {
+            console.log(err);
+        } else {
+            passport.authenticate("local")(req, res, function() {
+                res.redirect("/secrets");
+            });
+        }
+    });
 
     /**
      * Old Technique using Bcrypt (see above for Passport)
      */
     // const username = req.body.username;
-    // // Hash password using bcrypt
+    // Hash password using bcrypt
     // const password = req.body.password;
 
-    // // Mongoose will decrypt password at this stage
+    // Mongoose will decrypt password at this stage
     // User.findOne({
     //     email: username
     // }, function (err, foundUser) {
@@ -122,7 +163,7 @@ app.post("/login", function (req, res) {
     //         if (foundUser) {
     //             bcrypt.compare(password, foundUser.password, function(err, result) {
     //                 if (result === true) {
-    //                     // Password = db password
+                        // Password = db password
     //                     res.render("secrets");
     //                 }
     //             });
