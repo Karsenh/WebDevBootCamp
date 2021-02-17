4dd135f64743bb222d73627a44befaecccdbdb1c
Karsenh <KarsenHansen@gmail.com>
2021-02-16 21:17:33 -0800

    Added conditional rendering with Ternary and && operator

---
+++ S.33 - REACT/conditional-rendering/src/components/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Login from "./Login";
+
+var isLoggedIn = false;
+
+const currentTime = new Date().getHours();
+
+console.log(currentTime);
+
+function App() {
+  return (
+    <div className="container">
+      {currentTime > 10 && isLoggedIn === true ? (
+        <h1>Hello - Please Sign in.</h1>
+      ) : (
+        <Login />
+      )}
+    </div>
+  );
+}
+
+export default App;
